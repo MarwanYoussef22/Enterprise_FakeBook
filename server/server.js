ae7ddc5c27579029c46cd51ff21d7ec47f5b426e
Nadia <nadia@fake.com>
2023-10-11 14:20:53 -0400

    server set up

--- server/server.js
+++ server/server.js
@@ -1,20 +1,58 @@
+const { MongoClient } = require('mongodb');
 const express = require('express');
-const mongoose = require('mongoose');
+const { faker } = require('@faker-js/faker');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
-const app = express();
+console.log(faker);
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost/employee_directory', { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
+var url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+const dbName = "enterprise_fakebook";
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+var app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-// Define employee schema and model
-const employeeSchema = new mongoose.Schema({
-  name: String,
-  phoneNumber: String,
-  jobRole: String,
-  workLocation: String,
-  salary: Number,
+async function mongoConnect(collectionName) {
+    await client.connect();
+    const db = client.db(dbName);
+    return db.collection(collectionName);
+}
+
+app.get("/init", async (req, res) => {
+    const employee_directory = await mongoConnect('employee_directory');
+    const security_information = await mongoConnect('security_information');
+    
+    // await employee_directory.insertOne({
+    //     "id": 1, 
+    //     "name": "Alan D. Schnitzer",
+    //     "phone_number": faker.phone.number(),
+    //     "job_role": "Chief Executive Officer",
+    //     "work_location": faker.location.city(),
+    //     "salary": faker.finance.amount(65000, 200000,0, '$')
+    // })
+    
+    // await security_information.insertOne({
+    //     "employee_id": 1, 
+    //     "username": faker.internet.userName({firstName: "Alan", lastName: "Schnitzer"}),
+    //     "password": faker.internet.password(),
+    // });
+
+    // await employee_relations.insertOne({
+    //     "mangaer_employee_id": 1, 
+    //          "direct_reports": [
+    //              "fake_id_1",
+    //              "fake_id_2"
+    //          ]
+    // });
+
+    console.log("INSERTED CEO");
+    res.send("Inserted CEO");
+    client.close();
 });
+
+// start the rest service
+var port = 3000;
+console.log('service opening on port: ' + port)
+app.listen(port);

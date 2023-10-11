466a01a19cf12948d63ff8007349a4adcd42e160
MarwanYoussef22 <actionmon@yahoo.com>
2023-10-11 10:29:53 -0400

    add API endpoint

---
+++ routes/employee.js
@@ -0,0 +1,8 @@
+// routes/employee.js
+const express = require('express');
+const router = express.Router();
+const Employee = require('../models/employee');
+
+// Define routes for CRUD operations
+
+module.exports = router;

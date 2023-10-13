e49abaffc4fe7f4dfc87d111ea6a5f005bcdf728
Nadia <nadia@fake.com>
2023-10-13 10:32:29 -0400

    styling at a good checkpoint

--- enterprise-fakebook-client/src/EmployeeProfile.js
+++ enterprise-fakebook-client/src/EmployeeProfile.js
@@ -8,62 +8,51 @@ const EmployeeProfile = (props) => {
 
     const [employeeData, setEmployeeData] = useState(null);
     const [signedInEmployeeData, setSignedInEmployeeData] = useState(null);
-    // const [managerData, setManagerData] = useState(null);
+    const [managerData, setManagerData] = useState(null);
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
 
     const fetchData = async () => {
         if (choosenEmployee && signedInEmployee) {
-            await fetch(`/employees/${choosenEmployee}`, 
-            {
-                method: 'post',
-                headers: {'Content-Type':'application/json'},
-                body: JSON.stringify({
-                  "signedInEmployee": signedInEmployee
-                })
-            }).then((response) => response.json())
+            await fetch(`/employees/${choosenEmployee}`,
+                {
+                    method: 'post',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        "signedInEmployee": signedInEmployee
+                    })
+                }).then((response) => response.json())
                 .then((data) => {
                     setEmployeeData(data.employee);
+                    setSignedInEmployeeData(data.requestor);
+                    setManagerData(data.manager);
+                    setIsLoading(false);
                 })
                 .catch((error) => {
                     console.log(`Error getting employee data for employee id.: ${choosenEmployee}`, error);
                     setIsLoading(false);
                     setError('Cannot get employee data.');
                 });
-
-            // await fetch(`/employees/${signedInEmployee}`)
-            //     .then((response) => response.json())
-            //     .then((data) => {
-            //         setSignedInEmployeeData(data);
-            //     })
-            //     .catch((error) => {
-            //         console.log(`Error getting employee data for employee id.: ${signedInEmployee}`, error);
-            //         setIsLoading(false);
-            //         setError('Cannot get employee data.');
-            //     });
         }
-
-        // if (employeeData?.manager_id) {
-        //     await fetch(`/employees/${employeeData.manager_id}`)
-        //         .then((response) => response.json())
-        //         .then((data) => {
-        //             setManagerData(data);
-        //             setIsLoading(false);
-        //         })
-        //         .catch((error) => {
-        //             console.log(`Error getting manager data for employee id.: ${choosenEmployee}`, error);
-        //             setIsLoading(false);
-        //             setError('Cannot get manager data.');
-        //         });
-        // } else {
-        //     setIsLoading(false);
-        // }
     };
 
     useEffect(() => {
         setIsLoading(true);
         fetchData();
     }, [])
+
+    if (isLoading) {
+        return (
+            <div className='profile-container'>
+                <Link to="/search">
+                    <button>Back</button>
+                </Link>
+                <div className='profile-card'>
+                    <div className='loading'>Retrieving employee data.</div>
+                </div>
+            </div>
+        );
+    }
 
     return (
         <div className='profile-container'>
@@ -76,15 +65,17 @@ const EmployeeProfile = (props) => {
                     <p className='profile-name'>{employeeData?.first_name} {employeeData?.last_name}</p>
                     <p>{employeeData?.job_role}</p>
                     <p>{employeeData?.phone_number}</p>
-                    <p>  location: {employeeData?.work_location}</p>
-                    {signedInEmployeeData?.job === 'Human Resource Manager' || (employeeData?.manager && signedInEmployeeData?.id === employeeData?.manager) ? (
+                    <p>  Location: {employeeData?.work_location}</p>
+                    
+                    {/* <p>  test: {signedInEmployeeData?.work_location}</p> */}
+                    {signedInEmployeeData?.job_role === 'Human Resource Manager' || (employeeData?.manager && signedInEmployeeData?.id === employeeData?.manager) ? (
                         <p>  Salary: {employeeData?.salary}</p>
                     ) : null}
-                    {employeeData?.manager_id ? (
-                        <p>  manager ID: {employeeData?.manager_id}</p>
+                    {managerData?.first_name && managerData?.last_name ? (
+                        <p>  Manager: {managerData?.first_name} {managerData?.last_name}</p>
                     ) : null}
                 </div>
-                
+
                 {/* {employeeData?.manager_id && managerData ? (<div>
             <p> Manager: </p>
             <Employee
